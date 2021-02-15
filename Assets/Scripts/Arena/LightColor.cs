aa1d77a62b75d7b665ec4c708f9732346a51015d
David Alonso Santos <david.alonsosantos1@gmail.com>
2021-02-15 10:34:22 +0100

    add playe r2d  light

---
+++ Assets/Scripts/Arena/LightColor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightColor : MonoBehaviour
+{
+
+    public HardLight2D light2D;
+    public Color lightColor;
+    public ParticleSystem particleSys;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Material material = particleSys.GetComponent<ParticleSystemRenderer>().material;
+        Color color = material.color;
+        float alpha = light2D.Color.a;
+        lightColor = new Color(color.r, color.g, color.b, lightColor.a);
+        light2D.Color = lightColor;
+    }
+
+    
+}
