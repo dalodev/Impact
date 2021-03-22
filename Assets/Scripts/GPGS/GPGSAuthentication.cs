171dac12d10caa4f6c424d917869c2ec8736cd6d
David Alonso Santos <david.alonsosantos1@gmail.com>
2021-03-22 01:21:57 +0100

    added google play games service and authentication

---
+++ Assets/Scripts/GPGS/GPGSAuthentication.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using GooglePlayGames;
+using GooglePlayGames.BasicApi;
+using UnityEngine.SocialPlatforms;
+
+public class GPGSAuthentication : MonoBehaviour
+{
+    public static PlayGamesPlatform platform;
+
+    void Start()
+    {
+        if(platform == null)
+        {
+            PlayGamesClientConfiguration config = new PlayGamesClientConfiguration.Builder().RequestServerAuthCode(false).Build();
+            PlayGamesPlatform.InitializeInstance(config);
+            PlayGamesPlatform.DebugLogEnabled = true;
+            platform = PlayGamesPlatform.Activate();
+            Debug.Log("Play Games initialized");
+        }
+
+        PlayGamesPlatform.Instance.Authenticate(SignInInteractivity.CanPromptOnce, (success) =>
+        {
+            switch (success)
+            {
+                case SignInStatus.Success:
+                    Debug.Log("Play Games signed in succesfully");
+                    break;
+                default:
+                    Debug.Log("Play Games signin not sucess");
+                    break;
+
+            }
+
+        });
+    }
+
+}
